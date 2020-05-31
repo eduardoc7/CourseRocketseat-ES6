ef32ce81a251be958e2d787e0c34eb462f23ec9a
Eduardo Cordeiro <eduardo.cordeiro1@outlook.com>
2020-05-31 04:05:03 -0300

    creation aula operações em array

---
+++ conceitos/arraysoperations.js
@@ -0,0 +1,33 @@
+// o es contém algumas funções internas para facilitar as operações em arrays
+// no js antigo, isso era feito com for e muitas vezes precisando importar bibliotecas
+
+const arr = [5, 6, 7, 8, 4, 2]
+
+// map é uma função utilizada pra percorrer dentro do array
+// retornando o valor e o indice desse valor
+const newArr = arr.map(function(item, index){
+    return item + index
+});
+console.log(newArr);
+
+// reduce é uma função utilizada pra retornar uma única variável de todo o array
+// os parametros passados são: o valor atual e o proximo
+// somando todos os números e colocando em uma variável:
+const sum = arr.reduce(function(total, next){
+    return total + next
+});
+console.log(sum)
+
+// filter é uma função utilizada pra filtrar o que quisermos e marcar isso como falso, ou seja, nao mostrando 
+// ele irá retornar apenas os valores que se encaixam na condição, os que não se encaixam serão descartados
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+console.log(filter)
+
+// find é uma função utilizada pra percorrer dentro do array e verificar se esse valor existe dentro do array
+// retornando exatamente o valor que definido na condição 
+const find = arr.find(function(item){
+    return item === 5;
+});
+console.log(find)
