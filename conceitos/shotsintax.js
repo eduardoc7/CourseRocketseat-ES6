48cc4095837758b65b99542039644c9e69c30447
Eduardo Cordeiro <eduardo.cordeiro1@outlook.com>
2020-06-02 02:05:00 -0300

    creation aulas templateliterals and shortsintax

---
+++ conceitos/shotsintax.js
@@ -0,0 +1,10 @@
+const nome = 'Eduardo';
+const idade = 20;
+
+//modo simplificado para tornar menos repetitivo eliminando o nome: nome and idade: idade
+const usuario = {
+    nome,
+    idade,
+    cidade: 'Curitiba'
+};
+
---
+++ conceitos/templateliterals.js
@@ -0,0 +1,4 @@
+const nome = 'Eduardo';
+const idade = 20;
+
+console.log(`meu nome é ${nome} e tenho ${idade} anos`)
