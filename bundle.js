07e0e90c3a21b46fc9b86e3c2986020b23d48b66
Eduardo Cordeiro <eduardo.cordeiro1@outlook.com>
2020-06-10 17:16:47 -0300

    exercicios dos conceitos finalizados

--- bundle.js
+++ bundle.js
@@ -1,35 +1,59 @@
 "use strict";
 
-// 4.1 Desestruturação simples
-// A partir do seguinte objeto:
-var empresa = {
-  nome: 'Rocketseat',
-  endereco: {
-    cidade: 'Rio do Sul',
-    estado: 'SC'
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// A partir do array: 
+var arr = [1, 2, 3, 4, 5, 6]; // defina uma variável x que recebe a primeira
+// posição do vetor e outra variável y que recebe todo restante dos dados.
+
+console.log('5.1 - REST');
+var x = arr[0],
+    y = arr.slice(1);
+console.log(x);
+console.log(y);
+console.log('5.1 - REST'); // Crie uma função que recebe inúmeros parâmetros e retorna a soma de todos eles:
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
   }
-}; // Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis, no
-// fim deve ser possível fazer o seguinte:
 
-var nome = empresa.nome,
-    _empresa$endereco = empresa.endereco,
-    cidade = _empresa$endereco.cidade,
-    estado = _empresa$endereco.estado;
-console.log(nome); // Rocketseat
-
-console.log(cidade); // Rio do Sul
-
-console.log(estado); // SC
-// 4.2 Desestruturação em parâmetros
-// Na seguinte função:
-
-function mostraInfo(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  return "".concat(nome, " tem ").concat(idade, " anos.");
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-console.log(mostraInfo({
+;
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+
+console.log(soma(1, 2)); // 3
+
+console.log('5.3 - SPREAD'); // A partir do objeto e utilizando o operador spread:
+
+var usuario = {
   nome: 'Diego',
-  idade: 23
-}));
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
+  }
+}; // Crie uma variável usuario2 que contenha todos os dados do usuário porém com nome Gabriel.
+
+var usuario2 = _objectSpread(_objectSpread({}, usuario), {}, {
+  nome: 'Gabriel'
+});
+
+console.log(usuario2); // Crie uma variável usuario3 que contenha todos os dados do usuário porém com cidade Lontras
+
+var usuario3 = _objectSpread(_objectSpread({}, usuario), {}, {
+  endereco: {
+    cidade: 'Lontras'
+  }
+});
+
+console.log(usuario3);
---
+++ conceitos/exercicios/exe007.js
@@ -0,0 +1,14 @@
+// Utilize a sintaxe curta de objetos (Object Short Syntax) no seguinte objeto:
+
+const nome = 'Diego';
+const idade = 23;
+const usuario = {
+    nome: nome,
+    idade: idade,
+    cidade: 'Rio do Sul',
+};
+const usuario2 = {
+    nome,
+    idade,
+    cidade: 'Rio do Sul'
+};
